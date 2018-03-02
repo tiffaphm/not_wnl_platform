d585d727100a6881ce3e0d606bb9fa17ae50ab8e
Tiffany Pham <tiffanynpham@gmail.com>
2018-03-02 15:48:31 -0800

    config webpack

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-2'],
+        },
+      },
+      {
+        test: /\.css$/,
+        // include: /node_modules/,
+        loader: 'style-loader!css-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css'],
+  },
+};
