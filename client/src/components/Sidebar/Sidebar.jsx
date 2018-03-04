e7be2f69299b881c4dbac4f44705038dde348f42
Tiffany Pham <tiffanynpham@gmail.com>
2018-03-03 20:02:54 -0800

    change file name and add profile image

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import SidebarUserWelcome from './SidebarUserInfo';
 import '../styles/Sidebar.css';
 
 const Sidebar = () => (
   <div className="sidebar-container">
-    hi
+    <SidebarUserInfo />
   </div>
 );
 
---
+++ client/src/components/Sidebar/SidebarUserWelcome.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/Sidebar.css';
+
+const SidebarUserWelcome = () => (
+  <div className="sidebar-welcome-container">
+    <div className="sidebar-welcome-content">
+      <div className="sidebar-welcome-picture">
+        <img src="../assets/puppy.jpg"></img>
+      </div>
+    </div>
+  </div>
+);
+
+export default SidebarUserWelcome;
