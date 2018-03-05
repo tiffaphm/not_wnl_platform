fa808d638000e45299e017ed693577bedd68775c
Tiffany Pham <tiffanynpham@gmail.com>
2018-03-04 18:11:42 -0800

    add user chart with hardcoded data

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Sidebar from './Sidebar/Sidebar';
 import Summary from './DashboardSubComponents/Summary/Summary';
+import UserChart from './DashboardSubComponents/UserChart';
 import './styles/Dashboard.css';
 
 const Dashboard = () => (
   <div className="dashboard-container">
     <Sidebar />
     <Summary />
+    <UserChart />
   </div>
 );
 
---
+++ client/src/components/DashboardSubComponents/UserChart.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { AreaChart, Area, ResponsiveContainer, Tooltip } from 'recharts';
+import '../styles/UserChart.css';
+
+class UserChart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {name: 'Feb 20', amt: 50},
+        {name: 'Feb 21', amt: 40},
+        {name: 'Feb 22', amt: 105},
+        {name: 'Feb 23', amt: 75},
+        {name: 'Feb 24', amt: 35},
+        {name: 'Feb 25', amt: 80},
+        {name: 'Feb 26', amt: 96},
+      ],
+      colors: ['#FBDC4A', '#FFC900', '#FFE893'],
+    };
+  }
+
+  render() {
+    return (
+      <div className="user-chart-container">
+        <ResponsiveContainer width="100%" height="100%">
+          <AreaChart data={this.state.data} margin={{left: -4, right: 2}}>
+            <Tooltip />
+            <Area dataKey="amt" stroke={this.state.colors[0]} fill={this.state.colors[0]} strokeWidth={2} activeDot={{r: 8}} />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
+    );
+  }
+}
+
+export default UserChart;
