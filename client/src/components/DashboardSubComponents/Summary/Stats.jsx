aa76d293cf5c4ddc9177c5ab5dea7eb524e82e07
Tiffany Pham <tiffanynpham@gmail.com>
2018-03-04 16:42:52 -0800

    add placeholder for user rating and jobs worked

--- client/src/components/DashboardSubComponents/Summary/Stats.jsx
+++ client/src/components/DashboardSubComponents/Summary/Stats.jsx
@@ -3,7 +3,21 @@ import '../../styles/Summary.css';
 
 const Stats = () => (
   <div className="stats-container summary-view">
-    stats
+    <div className="stats-title">
+      <p>USER STATS</p>
+    </div>
+    <div className="stats-list-container">
+      <div className="stats-current-rating stats-entry">
+        <span>RATING</span>
+        <span className="stats-number">4.8</span>
+      </div>
+      <div className="stats-jobs-completed stats-entry">
+        <span>JOBS COMPLETED</span>
+        <span className="stats-number">35</span>
+        <span>DAYS WORKED</span>
+        <span className="stats-number">42</span>
+      </div>
+    </div>
   </div>
 );
 
